1c5d6708ec1c183731d3be6f48edcb385c10195a
jfranzone <jeffreyfranzone@msn.com>
2025-02-13 18:42:56 -0700

    fix failed test

--- src/GitHubActionsDotNet.Api.Tests/Models/WeatherForecastTests.cs
+++ src/GitHubActionsDotNet.Api.Tests/Models/WeatherForecastTests.cs
@@ -12,7 +12,7 @@ public class WeatherForecastTests
             TemperatureC = 0
         };
 
-        weatherForecast.TemperatureF.Should().Be(329);
+        weatherForecast.TemperatureF.Should().Be(32);
     }
 }
 
